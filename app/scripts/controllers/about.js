9576740ef01121afc7d7bf20e24af724735d431c
Erik Duindam <erik@erikduindam.com>
2016-01-20 14:36:56 -0500

    Initial application

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name vivieApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the vivieApp
+ */
+angular.module('vivieApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name vivieApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the vivieApp
+ */
+angular.module('vivieApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
